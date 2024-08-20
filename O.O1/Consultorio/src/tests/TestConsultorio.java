7ecbd1ce4ffa3fce28e570a3d643f7c34c3f03d1
Bono-Dev929 <bono.dev.22@gmail.com>
2024-08-20 09:51:05 -0300

    Update TestConsultorio.java
    agregue un output

--- O.O1/Consultorio/src/tests/TestConsultorio.java
+++ O.O1/Consultorio/src/tests/TestConsultorio.java
@@ -14,6 +14,7 @@ public class TestConsultorio {
 		Paciente paciente2 = new Paciente("Jose", "Fernandez", 1.75f, 87.2f);
 		
 		//Mostrando medico y pacientes con su imc
+		System.out.println("Visita 1");
 		System.out.println("Medico: "+medico1.traerNombreCompleto());
 		System.out.println("Paciente "+paciente1.traerNombreCompleto()+": IMC "+medico1.calcularIMC(paciente1));
 		System.out.println("Paciente "+paciente2.traerNombreCompleto()+": IMC "+medico1.calcularIMC(paciente2));
