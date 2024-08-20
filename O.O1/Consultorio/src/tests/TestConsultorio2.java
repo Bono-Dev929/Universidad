592e8943d4a8fb9f27a156c5d24ca150a91b0b3f
Bono-Dev929 <bono.dev.22@gmail.com>
2024-08-20 09:52:33 -0300

    Update TestConsultorio2.java
    agregue 2 outputs

--- O.O1/Consultorio/src/tests/TestConsultorio2.java
+++ O.O1/Consultorio/src/tests/TestConsultorio2.java
@@ -16,14 +16,17 @@ public class TestConsultorio2 {
 		
 		
 		//Mostrando medico y pacientes con su imc
+		System.out.println("Visita 1");
 		System.out.println("Medico: "+medico1.traerNombreCompleto());
 		System.out.println("Paciente "+paciente1.traerNombreCompleto()+": IMC "+medico1.calcularIMC(paciente1));
 		System.out.println("Paciente "+paciente2.traerNombreCompleto()+": IMC "+medico1.calcularIMC(paciente2));
 		System.out.println("\n");
+		
 		//modificando el peso
 		paciente1.actualizarPeso(-4);
 		paciente2.actualizarPeso(1);
 		//mostrando el nuevo imc
+		System.out.println("Visita 2");
 		System.out.println("Medico: "+medico1.traerNombreCompleto());
 		System.out.println("Paciente "+paciente1.traerNombreCompleto()+": IMC "+medico1.calcularIMC(paciente1));
 		System.out.println("Paciente "+paciente2.traerNombreCompleto()+": IMC "+medico1.calcularIMC(paciente2));
