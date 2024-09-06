e97b91b3cace7ff1f411d9c8c6e9009c1ea3076f
Bono-Dev929 <bono.dev.22@gmail.com>
2024-09-06 17:13:10 -0300

    Mejoras de codigo + Lista de generos
    Mejoras en el codigo, como dijo el profe, agregue la lista de generos, el genero de la pelicula se saca de ahi

--- O.O1/ProyectoIncaa/src/modelo/Incaa.java
+++ O.O1/ProyectoIncaa/src/modelo/Incaa.java
@@ -1,42 +1,47 @@
 package modelo;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-
-import modelo.*;
 
 public class Incaa {
 
 	//atributos
 	List<Pelicula> catalogo;
+	List<Genero> generos;
 
 	//constructor
 	public Incaa() {
 		this.catalogo = new ArrayList<Pelicula>();
+		this.generos = new ArrayList<Genero>();
 	}
 	
 	//metodos
-	public boolean agregarPelicula(String p,Genero g) {//mejorar y modificar
-		Pelicula peli = new Pelicula(0, p, g);//
+	public boolean agregarPelicula(String p,Genero g) {
+	
 		boolean existe = false,agregada = false;
-		int i = 0;
+		int i = 0, id = 1;
 		
-		while(i < catalogo.size()) {//
+		while(i < catalogo.size() && !existe) {
 			if (catalogo.get(i).getPelicula().equals(p)) {
-				i = catalogo.size();//
 				existe = true;
 			}
 			i += 1;
 		}
+		/*
+		 if(traerPelicula(p) != null){
+		 	throw new Exception("La pelicula ya existe");
+		 }
+		  **/
+		
+		//genero obtenido desde la lista de generos
+		
 		//si es false = true y si es true = false
 		if (!existe) {
-			if (catalogo.size() == 0) {//
-				catalogo.add(peli);//
-			}else {
-				peli.setIdPelicula(catalogo.get(catalogo.size()-1).getIdPelicula()+1);//
-				catalogo.add(peli);//
+			if (catalogo.size() != 0) {
+				id = catalogo.get(catalogo.size()-1).getIdPelicula()+1;	
 			}
+			Pelicula p1 = new Pelicula(id, p, g);
+			catalogo.add(p1);
 			agregada = true;
 		}
 		
@@ -46,13 +51,10 @@ public class Incaa {
 	public Pelicula traerPelicula(int idPelicula) {
 		
 		int i =0;
-		Pelicula p = new Pelicula(-1, null,null);
-		while(i<catalogo.size()) {
+		Pelicula p = null;
+		while(i<catalogo.size() && p == null) {
 			if(catalogo.get(i).getIdPelicula() == idPelicula) {
 				p = catalogo.get(i);
-				i = catalogo.size()+1;
-			}else {
-				p = null;
 			}
 			i +=1;
 		}
@@ -63,36 +65,29 @@ public class Incaa {
 	public List<Pelicula> traerPelicula(String pName) {
 		
 		List<Pelicula> listPelis = new ArrayList<>();
-		Pelicula p = new Pelicula(-1, null, null);
 		String[] aux;
 		
-		for (int i = 0; i < catalogo.size(); i++) {
-			aux = catalogo.get(i).getPelicula().split(" ");
-			for (int j = 0; j < aux.length; j++) {
-				if (aux[j].equals(pName)) {
-					p = catalogo.get(i);
-					listPelis.add(p);
+		for (Pelicula p : catalogo) {
+			aux = p.getPelicula().split(" ");
+			for (String s : aux) {
+				if (s.equals(pName)) {
+					Pelicula pe = p;
+					listPelis.add(pe);
 				}
 			}
 		}
-		
-		
-		
 		return listPelis;
 	}
 	
 	public List<Pelicula> traerPelicula(Genero g) {
 		
 		List<Pelicula> listPelis = new ArrayList<>();
-		Pelicula p = new Pelicula(-1, null, null);
 		Genero aux;
 		
-		for (int i = 0; i < catalogo.size(); i++) {
-			
-			aux = catalogo.get(i).getGenero();
-
+		for (Pelicula pelicula : catalogo) {
+			aux = pelicula.getGenero();
 			if (aux.equals(g)) {
-				p = catalogo.get(i);
+				Pelicula p = pelicula;
 				listPelis.add(p);
 			}
 		}
@@ -104,10 +99,11 @@ public class Incaa {
 		
 		if (this.traerPelicula(idPelicula) == null) {
 			throw new Exception("La pelicula con id {"+idPelicula+"} no existe");
-		}else {
-			catalogo.get(idPelicula).setPelicula(pelicula);
-			catalogo.get(idPelicula).setGenero(g);
 		}
+		
+		catalogo.get(idPelicula-1).setPelicula(pelicula);
+		catalogo.get(idPelicula-1).setGenero(g);
+		
 		
 	}
 	
@@ -115,9 +111,63 @@ public class Incaa {
 		
 		if (this.traerPelicula(idPelicula) == null) {
 			throw new Exception("La pelicula con id {"+idPelicula+"} no existe");
-		}else {
-			catalogo.remove(idPelicula);
 		}
+		
+		catalogo.remove(idPelicula-1);
+		
+	}
+	
+	public boolean agregarGenero(String g) {
+		
+		int i = 0, id=1;
+		boolean v = false;
+		
+		while(i<generos.size() && !v) {
+			if(generos.get(i).getGenero().equals(g)) {
+				v=true;
+			}
+			i++;
+		}
+		if(!v) {
+			if (generos.size() != 0) {
+				id = generos.get(generos.size()-1).getIdGenero()+1;	
+			}
+			Genero ge = new Genero(id, g);
+			generos.add(ge);
+			v = true;
+		}
+		
+		return v;
+	}
+	
+	public Genero traerGenero(String g) {
+		
+		Genero ge = null;
+		int i =0;
+		
+		while(i<generos.size() && ge == null) {
+			if(generos.get(i).getGenero().equals(g)) {
+				ge = generos.get(i);
+			}
+			i +=1;
+		}
+		
+		return ge;
+	}
+	
+	public Genero traerGenero(int idGen) {
+		
+		Genero ge = null;
+		int i =0;
+		
+		while(i<generos.size() && ge == null) {
+			if(generos.get(i).getIdGenero()== idGen) {
+				ge = generos.get(i);
+			}
+			i +=1;
+		}
+		
+		return ge;
 	}
 	
 	//accesores
@@ -128,16 +178,17 @@ public class Incaa {
 		this.catalogo = catalogo;
 	}
 	
+	public List<Genero> getGeneros() {
+		return generos;
+	}
+	
+	public void setGeneros(List<Genero> generos) {
+		this.generos = generos;
+	}
+	
 	@Override
 	public String toString() {
-		String cata = "";
-		
-		for (int i = 0; i < catalogo.size(); i++) {
-			cata += catalogo.get(i).toString()+"\n";
-		}
-		
-		cata +=". . .";
-		return cata;
+		return "Catalogo["+catalogo+"]\nGeneros["+generos+"]";
 	}
 	
 	
